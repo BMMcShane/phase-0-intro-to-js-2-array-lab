17700745150082116d19aa3e22f9379050b3fdca
Ben McShane <benjaminmartinmc@gmail.com>
2022-08-09 14:31:21 -0600

    commit tree f2992e97499ffb22d54cb3f67aee91716639517c

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your solution here!
+let cats = ['Milo', 'Otis', 'Garfield'];
+let newCats;
+
+function destructivelyAppendCat(name) {
+    return cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+    return cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat(name) {
+    return cats.pop(name);
+}
+
+function destructivelyRemoveFirstCat(name) {
+    return cats.shift(name);
+}
+
+function appendCat(name) {
+    return newCats = [...cats, name];
+}
+
+function prependCat(name) {
+    return newCats = [name, ...cats];
+}
+
+function removeLastCat() {
+    return cats.slice(0,2);
+}
+
+function removeFirstCat() {
+    return cats.slice(1);
+}
